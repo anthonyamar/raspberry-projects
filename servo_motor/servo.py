6b2b1a3ef0be7de5f82c6e8ece7380f5cdaa0163
anthonyamar <anthonyamar@labodessons.fr>
2019-03-29 22:01:50 +0100

    try keyboard event

--- servo_motor/servo.py
+++ servo_motor/servo.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO     # Importing RPi library to use the GPIO pins
 from time import sleep      # Importing sleep from time library to add delay in code
+import keyboard
 
 servo_pin = 21      # Initializing the GPIO 21 for servo motor
 
@@ -7,16 +8,28 @@ GPIO.setmode(GPIO.BCM)          # We are using the BCM pin numbering
 GPIO.setup(servo_pin, GPIO.OUT)     # Declaring GPIO 21 as output pin
 
 p = GPIO.PWM(servo_pin, 50)     # Created PWM channel at 50Hz frequency
-p.start(2.5)    
+p.start(2.5) 
+
+def right():
+	p.ChangeDutyCycle(0.5)
+	
+def left():
+	p.ChangeDutyCycle(0.5)
+
 
 try:
 	while 1:                    # Loop will run forever
-		p.ChangeDutyCycle(2.5)  # Move servo to 0 degrees
-		sleep(1)                # Delay of 1 sec
-		p.ChangeDutyCycle(7.5)  # Move servo to 90 degrees
-		sleep(1)                
-		p.ChangeDutyCycle(12.5) # Move servo to 180 degrees
-		sleep(1)
+		if keyboard.is_pressed('d'): right()
+			
+		if keyboard.is_pressed('q'): left()
+			
+#		p.ChangeDutyCycle(0.5)  # Move servo to 0 degrees
+#		sleep(1)                # Delay of 1 sec
+#		p.ChangeDutyCycle(7.5)  # Move servo to 90 degrees
+#		sleep(1)                
+#		p.ChangeDutyCycle(12.5) # Move servo to 180 degrees
+#		sleep(1)
+
 		
 # If Keyborad Interrupt (CTRL+C) is pressed
 except KeyboardInterrupt:
